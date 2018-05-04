4828e62fb22dff1dbd97a0cdeb7ec557d27c0f06
garrach achraf <achraf.garrach@esprit.tn>
2018-05-04 12:01:36 +0100

    update connection string

--- Website/BackEnd/database_connector/connector.js
+++ Website/BackEnd/database_connector/connector.js
@@ -1,7 +1,4 @@
 var mongoose = require('mongoose');
-var uri = "mongodb://mongo-admin:270bytes@270bytescluster-shard-00-00-6dhzp.mongodb.net:27017," +
-    "270bytescluster-shard-00-01-6dhzp.mongodb.net:27017," +
-    "270bytescluster-shard-00-02-6dhzp.mongodb.net:27017/weather?" +
-    "ssl=true&replicaSet=270bytesCluster-shard-0&authSource=admin";
+var uri = "mongodb://mongo-admin:270bytes@cluster0-shard-00-00-aoxkb.mongodb.net:27017,cluster0-shard-00-01-aoxkb.mongodb.net:27017,cluster0-shard-00-02-aoxkb.mongodb.net:27017/weather?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 mongoose.connect(uri);
